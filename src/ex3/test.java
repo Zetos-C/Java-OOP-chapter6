2d227b5865ec4a62bd81e99a50d29d22377eaa3a
duongdat <duongdat030@gmail.com>
2024-02-28 10:52:30 +0700

    Update ex3

--- src/ex3/test.java
+++ src/ex3/test.java
@@ -3,8 +3,10 @@ package ex3;
 public class test {
 	public static void main(String[] args) {
 		Movable m = new MovablePoint(3, 4, 5, 6);
+		System.out.println("Initial position: " + m);
 		m.moveUp();
+		System.out.println("After moving up: " + m);
 		m.moveRight();
-		System.out.println(m);
+		System.out.println("After moving right: " + m);
 	}
 }
